47e959e7e9a959ce283941f9323b630d221e7e39
Augusto Vicente <devguto@gmail.com>
2023-08-04 08:08:37 -0300

    progress for intro

--- frontend/src/pages/FillInformation/index.tsx
+++ frontend/src/pages/FillInformation/index.tsx
@@ -1,77 +1,68 @@
 import { useState } from "react";
-import { LoginContainer } from "./styles";
-import { useAuth } from "contexts/auth.context";
-import { ByCapitel } from "components/ByCapitel/ByCapitel";
+import { FillInfoContainer, IntroContainer } from "./styles";
 
 export const FillInformation = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [seePwd, setSeePwd] = useState(false);
-    const { signIn } = useAuth();
-    
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        setLoading(true);
-        event.preventDefault();
-        const response = await signIn({
-            email: username,
-            password
-        });
-        setLoading(false);
-        
-        if (!response)
-        {
-            setError(true);
-            return;
-        }
+    const [step, setStep] = useState(0);
+    const [userType, setUserType] = useState<number|null>(null);
+
+    const handleUserType = (userType: number) => {
+        setUserType(userType);
+        setStep(0);
     };
-    return (
-        <LoginContainer>
-            <img className="background" src="imgs/background.png" alt="Background Login" />
-            <div className="login-content">
-                <img className="logo" src="imgs/hacktown.png" alt="Hacktown Logo" />
-                <form onSubmit={handleSubmit}>
-                    <div className={`form-group ${error ? 'error': ''}`}>
-                        <label htmlFor="username">E-mail</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className={`form-group ${error ? 'error': ''}`}>
-                        <label htmlFor="password">Senha</label>
-                        <div className="pwd-content">
-                            <input
-                                type={seePwd ? 'text' : 'password'}
-                                className="form-control"
-                                id="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            {seePwd ? (
-                                <i className="ph ph-eye-slash" onClick={() => setSeePwd(false)}></i>
-                            ) : (
-                                <i className="ph ph-eye" onClick={() => setSeePwd(true)}></i>
-                            )}
-                        </div>
-                        {error && (
-                            <div className="alert alert-danger" role="alert">
-                                Senha ou email incorretos.
-                            </div>
-                        )}
-                    </div>
-                    <button type="submit" className="btn">
-                        Entrar
-                        {loading && (
-                            <img src="imgs/spinner.webp" />)}
-                    </button>
-                </form>
-                <ByCapitel theme="dark" />
+    
+    return !userType ? (
+        <IntroContainer step={step}>
+            <div className="step-1">
+                <img src="imgs/intro.png" alt="Dunnit Intro" />
             </div>
-        </LoginContainer>
+            <div className="step-2">
+                <span>Bem vindx a</span>
+                <span>LET'S DUNIT</span>
+            </div>
+            <div className="step-3">
+                <img src="imgs/dunit.png" alt="" />
+                <div>
+                    <span>Para ajudar quem quer</span>
+                    <span>investimento</span>
+                </div>
+                <div>
+                    <span>ou para quem quer</span>
+                    <span>investir</span>
+                </div>
+            </div>
+            <div className="step-4">
+                <img src="imgs/dunit.png" alt="" />
+                <span>Mas vamos lá!</span>
+                <span>Você é:</span>
+                <button onClick={() => handleUserType(2)}>
+                    Empreendedor
+                </button>
+                <button onClick={() => handleUserType(1)}>
+                    Investidor
+                </button>
+            </div>
+        </IntroContainer>
+    ) : (
+        <FillInfoContainer>
+            <div className="step-1">
+                <img src="imgs/dunit.png" alt="" />
+                <span>
+                    {userType === 1 ? 'Investidor ✏' : 'StartUp ✨'}
+                </span>
+                <span>
+                    {userType === 1 ? 'Legal! Vamos te ajudar nesse fit para aporte!' : 'Legal!'}
+                </span>
+                <span>
+                    Agora precisamos de algumas informações da sua empresa!
+                </span>
+                <span>
+                    Vamos lá?
+                </span>
+            </div>
+            <div className="step-2">
+                <img src="imgs/dunit.png" alt="" />
+
+            </div>
+        </FillInfoContainer>
     );
 }
